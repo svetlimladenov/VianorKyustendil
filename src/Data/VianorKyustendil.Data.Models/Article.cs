0d67cc3ce9ecb9a399d578519cad8edfa4c43073
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-05-10 01:12:40 +0300

    Created Logo and added it to the navbar.

---
+++ src/Data/VianorKyustendil.Data.Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VianorKyustendil.Data.Models
+{
+    public class Article
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public VianorKyustendilUser Author { get; set; }
+
+
+    }
+}
---
+++ src/Data/VianorKyustendil.Data/Migrations/20190508162038_ArticlesAdd.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VianorKyustendil.Data.Migrations
+{
+    public partial class ArticlesAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    CoverImageUrl = table.Column<string>(nullable: true),
+                    AuthorId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
